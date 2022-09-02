c795b3b0aff0b2f50a1f9cf456e34d99534e1dbe
Keepon-blip <skmephep@gmail.com>
2022-09-02 09:20:22 +0300

    Feat: Fragments

--- app/src/main/java/com/example/foodapp/IntroActivity.kt
+++ app/src/main/java/com/example/foodapp/IntroActivity.kt
@@ -1,11 +1,23 @@
 package com.example.foodapp
 
+import android.content.Intent
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class IntroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val secondsDelayed = 1
+        Handler().postDelayed({
+            startActivity(Intent(this@IntroActivity, MainActivity::class.java))
+            finish()
+        }, (secondsDelayed * 1400).toLong())
     }
+
+
+
 }
